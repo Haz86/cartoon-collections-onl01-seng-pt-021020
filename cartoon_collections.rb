a05e5307180e4319c744025f9fc9370332412f0f
Hussain Ahmed <haz6798@icloud.com>
2020-02-23 02:05:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(dwarves)
   
   
 def summon_captain_planet(planeteer_calls)
-  puts planeteer_calls.collect{|call| call.capitalize + "!"}
+  planeteer_calls.map { |call| call.capitalize + "!" }
 end
 
 def long_planeteer_calls(characters)
