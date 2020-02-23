cad905ac67f1a64d30720790f5327ca27daa8393
Hussain Ahmed <haz6798@icloud.com>
2020-02-23 01:59:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,8 +3,8 @@ dwarves.each_with_index do |dwarf, index|
   puts "#{index+1}.#{drarf}"
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  puts planeteer_calls.collect{|call| call.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
